a40973f8c050b5306852c4a24bfaa56b39b9aad2
medaminecheikh <amine_cheikhh@outlook.fr>
2023-02-13 14:25:49 +0100

    impl

--- src/main/java/com/administration/service/ProfileServiceImpl.java
+++ src/main/java/com/administration/service/ProfileServiceImpl.java
@@ -3,11 +3,14 @@ package com.administration.service;
 import com.administration.dto.ProfileRequestDTO;
 import com.administration.dto.ProfileResponseDTO;
 import com.administration.dto.ProfileUpdateDTO;
+import com.administration.entity.Profile;
 import com.administration.mappers.ProfileMapper;
 import com.administration.repo.ProfileRepo;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class ProfileServiceImpl implements ProfileService{
@@ -21,22 +24,34 @@ public class ProfileServiceImpl implements ProfileService{
 
     @Override
     public ProfileResponseDTO addProfile(ProfileRequestDTO RequestDTO) {
-        
-        return null;
+        Profile profile=profileMapper.ProfileRequestDTOProfile(RequestDTO);
+        profile.setIdProfile(UUID.randomUUID().toString());
+        Profile profilesave=profileRepo.save(profile);
+        ProfileResponseDTO profileResponseDTO=profileMapper.ProfileTOProfileResponseDTO(profilesave);
+        return profileResponseDTO;
     }
 
     @Override
     public ProfileResponseDTO getProfile(String id) {
-        return null;
+        Profile profile= profileRepo.findById(id).get();
+        ProfileResponseDTO profileResponseDTO=profileMapper.ProfileTOProfileResponseDTO(profile);
+        return profileResponseDTO;
     }
 
     @Override
     public List<ProfileResponseDTO> listProfiles() {
-        return null;
+        List<Profile> profiles=profileRepo.findAll();
+        List<ProfileResponseDTO> profileResponseDTOList= profiles.stream()
+                .map(profile -> profileMapper.ProfileTOProfileResponseDTO(profile))
+                .collect(Collectors.toList());
+        return profileResponseDTOList;
     }
 
     @Override
     public void updateProfileDTO(ProfileUpdateDTO dto) {
+        Profile profile= profileRepo.findById(dto.getIdProfile()).get();
+        profileMapper.updateProfileFromDto(dto,profile);
+        profileRepo.save(profile);
 
     }
 }
