68d1460fd14fe173c46d765276785cb5c7c1de16
medaminecheikh <amine_cheikhh@outlook.fr>
2023-05-29 16:37:02 +0100

    caisse

--- src/main/java/com/administration/security/Jwt/JwtVariables.java
+++ src/main/java/com/administration/security/Jwt/JwtVariables.java
@@ -1,7 +1,7 @@
 package com.administration.security.Jwt;
 
 public class JwtVariables {
-    public static final String SECRET="is secret :^D";
+    public static final String SECRET="is a secret :^D";
     public static final String AUTH_HEADER="Authorization";
     public static final long EXPIRE_ACCESS=800*60*1000; // 4h
     public static final long EXPIRE_REFRESH=600*60*1000; //10h
